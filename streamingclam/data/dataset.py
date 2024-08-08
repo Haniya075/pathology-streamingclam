a8d07a573c022fea7b7733d484a9f6f24361bd16
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-08-08 10:42:51 +0500

    Update dataset.py

--- streamingclam/data/dataset.py
+++ streamingclam/data/dataset.py
@@ -86,7 +86,7 @@ class StreamingClassificationDataset(Dataset):
 
             included["images"].append(images[0])
             included["labels"].append(label)
-            print("INCLUDED : ",included)
+            #print("INCLUDED : ",included)
 
             if self.mask_dir:
                 included["masks"].append(images[1])
