558a6a71f281c8b696f0b94df988f9a52027cc13
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-08-09 13:26:40 +0500

    Update dataset.py

--- streamingclam/data/dataset.py
+++ streamingclam/data/dataset.py
@@ -179,7 +179,7 @@ class StreamingClassificationDataset(Dataset):
 
         to_tensor = A.Compose([A.ToTensor(transpose_mask=True)], additional_targets={'mask': 'mask'}, is_check_shapes=False)
         print("Tensor made ")
-        sample = to_tensor(**sample)
+        #sample = to_tensor(**sample)
         print("Workss ")
 
         #if "mask" in sample.keys():
